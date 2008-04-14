d660941be37673dab746449c9deaf60ccb8d2612
Manuel Cerón <ceronman@gmail.com>
2008-04-14 16:47:02 -0500

    Added copyright notes and encoding configuration

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 conf = {
  'PREFIX':		'/opt/parallel',
  'MODULESET_PATH':	'/home/ceronman/mono/moduleset.py'
--- moduleset.py
+++ moduleset.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 class MonoModule(SVNModule):
 	repository = 'svn://svn.myrealbox.com/source/trunk' 
 
